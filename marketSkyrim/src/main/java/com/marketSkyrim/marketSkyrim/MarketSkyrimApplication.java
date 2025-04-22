e32eace0dee12c3a9e4b49b68dc30dd12750d7d7
pxxmdr <pedro.reis_04@hotmail.com>
2025-04-22 19:21:47 -0300

    Primeiro commit

---
+++ marketSkyrim/src/main/java/com/marketSkyrim/marketSkyrim/MarketSkyrimApplication.java
@@ -0,0 +1,13 @@
+package com.marketSkyrim.marketSkyrim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketSkyrimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketSkyrimApplication.class, args);
+	}
+
+}
---
+++ marketSkyrim/src/test/java/com/marketSkyrim/marketSkyrim/MarketSkyrimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marketSkyrim.marketSkyrim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketSkyrimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
